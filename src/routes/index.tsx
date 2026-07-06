e4e5f12580d3478db8ee7bfc5d260b14796f42fa
vtecbusinessgroup <vtecbusinessgroup@gmail.com>
2026-07-06 06:50:34 +0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
-// IMPORTANT: Ensure this path correctly points to where your ChatBot.tsx file is located!
-// It might be "../components/ChatBot" or just "../ChatBot" depending on your folders.
-import { ChatBot } from "../components/ChatBot"; 
 
 export const Route = createFileRoute("/")({
   component: () => (
     <div style={{ width: "100%", height: "100vh", overflow: "hidden", position: "relative" }}>
-      
-      {/* 1. This loads your 4,200-line masterpiece */}
       <iframe 
         src="/site.html" 
         style={{ width: "100%", height: "100%", border: "none", display: "block" }} 
         title="VTEC Business Group"
       />
-
-      {/* 2. This loads your original React Chatbot on top of the site */}
-      <ChatBot />
-      
     </div>
   ),
 });
-
